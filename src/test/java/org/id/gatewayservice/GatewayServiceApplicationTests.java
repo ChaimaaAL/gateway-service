7b878071d83f292784098572fb7cd71c1c9b123f
ChaimaaAL <aitlarbi.chaimaa@gmail.com>
2022-12-23 22:13:28 +0100

    Initial commit

---
+++ src/test/java/org/id/gatewayservice/GatewayServiceApplicationTests.java
@@ -0,0 +1,14 @@
+package org.id.gatewayservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+
+}
